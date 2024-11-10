dd3d2cbc0cb156717b36fce6b24ba4a50c8a518c
PaperMtn <andrewbyford86@hotmail.com>
2024-11-10 20:59:01 +0000

    Additional tests for new utils functions

--- tests/unit/test_unit_utils.py
+++ tests/unit/test_unit_utils.py
@@ -8,11 +8,12 @@ from gitlab_watchman.utils import (
     convert_to_epoch,
     convert_to_utc_datetime,
     deduplicate_results,
-    convert_to_dict
+    convert_to_dict,
+    split_to_chunks
 )
 
 
-def test_convert_to_epoch():
+def test_convert_to_epoch_string():
     # Test with a correct ISO 8601 timestamp containing seconds
     string_timestamp = '2021-09-20T10:00:00.000+00:00'
     expected_output = 1632132000
@@ -42,6 +43,34 @@ def test_convert_to_epoch():
     string_timestamp = None
     expected_output = None
     assert convert_to_epoch(string_timestamp) == expected_output
+
+
+def test_convert_to_epoch_datetime():
+    # Test an int is returned when passing a datetime object
+    assert isinstance(convert_to_epoch(datetime.now()), int)
+
+    # Test with None input - Should gracefully fail and return None
+    string_timestamp = None
+    expected_output = None
+    assert convert_to_epoch(string_timestamp) == expected_output
+
+    # Test correct epoch timestamp is returned for datetime object
+    string_timestamp = '2021-09-20T10:00:00.000+00:00'
+    dt_input = datetime.strptime(string_timestamp, '%Y-%m-%dT%H:%M:%S.%f%z')
+    expected_output = 1632132000
+    assert convert_to_epoch(dt_input) == expected_output
+
+    # Test correct epoch timestamp is returned for datetime object with different timezone
+    string_timestamp = '2021-09-20T15:00:00.000+05:00'
+    dt_input = datetime.strptime(string_timestamp, '%Y-%m-%dT%H:%M:%S.%f%z')
+    expected_output = 1632132000
+    assert convert_to_epoch(dt_input) == expected_output
+
+    # Test correct epoch timestamp is returned for datetime object with different timezone
+    string_timestamp = '2021-09-20T05:00:00.000-05:00'
+    dt_input = datetime.strptime(string_timestamp, '%Y-%m-%dT%H:%M:%S.%f%z')
+    expected_output = 1632132000
+    assert convert_to_epoch(dt_input) == expected_output
 
 
 def test_convert_to_utc_datetime():
@@ -81,6 +110,11 @@ def test_convert_to_utc_datetime():
     # Test with None input - Should gracefully fail and return None
     string_timestamp = None
     expected_output = None
+    assert convert_to_utc_datetime(string_timestamp) == expected_output
+
+    # Test with YYYY-MM-DD input - Should return a datetime object with the timezone set to UTC
+    string_timestamp = '2021-09-20'
+    expected_output = datetime(2021, 9, 20)
     assert convert_to_utc_datetime(string_timestamp) == expected_output
 
 
@@ -166,3 +200,18 @@ def test_deduplicate_results(simple_example_result: Dict[Any, Any],
     # Test with multiple dataclass examples with duplicates
     assert (deduplicate_results([dataclass_example_result_one, dataclass_example_result_one]) ==
             [convert_to_dict(dataclass_example_result_one)])
+
+
+def test_split_to_chunks():
+    # Define test cases
+    test_cases = [
+        ([1, 2, 3, 4, 5, 6, 7], 3, [[1, 4, 7], [2, 5], [3, 6]]),
+        ([1, 2, 3, 4, 5, 6], 2, [[1, 3, 5], [2, 4, 6]]),
+        ([1, 2, 3, 4], 4, [[1], [2], [3], [4]]),
+        ([1, 2, 3], 1, [[1, 2, 3]]),
+        ([], 3, [[], [], []])  # Edge case: empty input list
+    ]
+
+    for input_list, no_of_chunks, expected in test_cases:
+        result = list(split_to_chunks(input_list, no_of_chunks))
+        assert result == expected, f"Failed for input_list={input_list}, no_of_chunks={no_of_chunks}"
