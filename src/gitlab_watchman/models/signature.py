fa2988d1c18913e8eb4de11cefb19d12b28724c8
PaperMtn <andrewbyford86@hotmail.com>
2024-11-11 08:33:46 +0000

    Unit tests added for signatures

--- src/gitlab_watchman/models/signature.py
+++ src/gitlab_watchman/models/signature.py
@@ -1,25 +1,7 @@
+import datetime
 from typing import Any, Dict
 from dataclasses import dataclass
-
-
-@dataclass(slots=True)
-class Signature(object):
-    """ Class that handles loaded signature objects. Signatures
-    define what to search for in GitLab and where to search for it.
-    They also contain regex patterns to validate data that is found"""
-
-    name: str
-    status: bool
-    author: str
-    date: str
-    version: str
-    description: str
-    severity: int
-    watchman_apps: list
-    scope: list
-    test_cases: dataclass
-    search_strings: str
-    patterns: str
+from typing import List
 
 
 @dataclass(slots=True)
@@ -28,43 +10,50 @@ class TestCases(object):
     fail_cases: list
 
 
-# def load_from_yaml(sig_path: pathlib.PosixPath) -> list[Signature]:
-#     """Load YAML file and return a Signature object
-#
-#     Args:
-#         sig_path: Path of YAML file
-#     Returns:
-#         Signature object with fields populated from the YAML
-#         signature file
-#     """
-#
-#     with open(sig_path) as yaml_file:
-#         yaml_import = yaml.safe_load(yaml_file)
-#
-#         output = []
-#         for sig in yaml_import.get('signatures'):
-#             if 'gitlab' in sig.get('watchman_apps'):
-#                 output.append(
-#                     Signature(
-#                         name=sig.get('name'),
-#                         status=sig.get('status'),
-#                         author=sig.get('author'),
-#                         date=sig.get('date'),
-#                         version=sig.get('version'),
-#                         description=sig.get('description'),
-#                         severity=sig.get('severity'),
-#                         watchman_apps=sig.get('watchman_apps'),
-#                         scope=sig.get('watchman_apps').get('gitlab').get('scope'),
-#                         test_cases=TestCases(
-#                             match_cases=sig.get('test_cases').get('match_cases'),
-#                             fail_cases=sig.get('test_cases').get('fail_cases')
-#                         ),
-#                         search_strings=sig.get('watchman_apps').get('gitlab').get('search_strings'),
-#                         patterns=sig.get('patterns')
-#                     )
-#                 )
-#
-#     return output
+@dataclass(frozen=True, slots=True)
+class Signature:
+    """ Class that handles loaded signature objects. Signatures
+    define what to search for in Slack and where to search for it.
+    They also contain regex patterns to validate data that is found"""
+
+    name: str
+    status: str
+    author: str
+    date: str | datetime.date | datetime.datetime
+    version: str
+    description: str
+    severity: int or str
+    watchman_apps: Dict[str, Any]
+    scope: List[str]
+    test_cases: TestCases
+    search_strings: List[str]
+    patterns: List[str]
+
+    def __post_init__(self):
+        if self.name and not isinstance(self.name, str):
+            raise TypeError(f'Expected `name` to be of type str, received {type(self.name).__name__}')
+        if self.status and not isinstance(self.status, str):
+            raise TypeError(f'Expected `status` to be of type str, received {type(self.status).__name__}')
+        if self.author and not isinstance(self.author, str):
+            raise TypeError(f'Expected `author` to be of type str, received {type(self.author).__name__}')
+        if self.date and not (isinstance(self.date, datetime.date)
+                              or isinstance(self.date, str)
+                              or isinstance(self.date, datetime.datetime)):
+            raise TypeError(f'Expected `date` to be of type str, received {type(self.date).__name__}')
+        if self.version and not isinstance(self.version, str):
+            raise TypeError(f'Expected `version` to be of type str, received {type(self.version).__name__}')
+        if self.description and not isinstance(self.description, str):
+            raise TypeError(f'Expected `description` to be of type str, received {type(self.description).__name__}')
+        if self.severity and not (isinstance(self.severity, int) or isinstance(self.severity, str)):
+            raise TypeError(f'Expected `severity` to be of type int or str, received {type(self.severity).__name__}')
+        if self.scope and not isinstance(self.scope, list):
+            raise TypeError(f'Expected `scope` to be of type list, received {type(self.scope).__name__}')
+        if self.search_strings and not isinstance(self.search_strings, list):
+            raise TypeError(
+                f'Expected `search_strings` to be of type list, received {type(self.search_strings).__name__}')
+        if self.patterns and not isinstance(self.patterns, list):
+            raise TypeError(f'Expected `patterns` to be of type list, received {type(self.patterns).__name__}')
+
 
 def create_from_dict(signature_dict: Dict[str, Any]) -> Signature:
     """ Create a Signature object from a dictionary
@@ -84,11 +73,10 @@ def create_from_dict(signature_dict: Dict[str, Any]) -> Signature:
         description=signature_dict.get('description'),
         severity=signature_dict.get('severity'),
         watchman_apps=signature_dict.get('watchman_apps'),
-        scope=signature_dict.get('watchman_apps').get('gitlab').get('scope'),
+        scope=signature_dict.get('watchman_apps', {}).get('gitlab', {}).get('scope'),
         test_cases=TestCases(
-            match_cases=signature_dict.get('test_cases').get('match_cases'),
-            fail_cases=signature_dict.get('test_cases').get('fail_cases')
+            match_cases=signature_dict.get('test_cases', {}).get('match_cases'),
+            fail_cases=signature_dict.get('test_cases', {}).get('fail_cases')
         ),
-        search_strings=signature_dict.get('watchman_apps').get('gitlab').get('search_strings'),
-        patterns=signature_dict.get('patterns')
-    )
+        search_strings=signature_dict.get('watchman_apps', {}).get('gitlab', {}).get('search_strings'),
+        patterns=signature_dict.get('patterns'))
--- tests/unit/models/fixtures.py
+++ tests/unit/models/fixtures.py
@@ -12,7 +12,8 @@ from gitlab_watchman.models import (
     project,
     snippet,
     user,
-    wiki_blob
+    wiki_blob,
+    signature
 )
 
 
@@ -473,6 +474,38 @@ class GitLabMockData:
         "group_id": None
     }
 
+    MOCK_SIGNATURE_DICT = {
+        'name': 'Akamai API Access Tokens',
+        'status': 'enabled',
+        'author': 'PaperMtn',
+        'date': '2023-12-22',
+        'description': 'Detects exposed Akamai API Access tokens',
+        'severity': '90',
+        'notes': None,
+        'references': None,
+        'watchman_apps': {
+            'gitlab': {
+                'scope': [
+                    'blobs'
+                ],
+                'search_strings': [
+                    'akab-'
+                ]
+            }
+        },
+        'test_cases': {
+            'match_cases': [
+                'client_token: akab-rWdcwwASNbe9fcGk-00qwecOueticOXxA'
+            ],
+            'fail_cases': [
+                'host: akab-fakehost.akamaiapis.net'
+            ]
+        },
+        'patterns': [
+            'akab-[0-9a-zA-Z]{16}-[0-9a-zA-Z]{16}'
+        ]
+    }
+
 
 @pytest.fixture
 def mock_commit():
@@ -532,3 +565,7 @@ def mock_user():
 @pytest.fixture
 def mock_wiki_blob():
     return wiki_blob.create_from_dict(GitLabMockData.MOCK_WIKI_BLOB_DICT)
+
+@pytest.fixture
+def mock_signature():
+    return signature.create_from_dict(GitLabMockData.MOCK_SIGNATURE_DICT)
